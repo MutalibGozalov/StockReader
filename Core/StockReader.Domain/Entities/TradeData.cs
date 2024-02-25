b1b6a838e6d02336545413c453c9bcd11d6b281b
Mutalib <mutalib.shg@code.edu.az>
2024-02-25 12:35:07 +0400

    Project Entity created

---
+++ Core/StockReader.Domain/Entities/TradeData.cs
@@ -0,0 +1,19 @@
+﻿using StockReader.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockReader.Domain.Entities
+{
+    public class TradeData : BaseEntity
+    {
+        public DateOnly Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public long Volume { get; set; }
+    }
+}
